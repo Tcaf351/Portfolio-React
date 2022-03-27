783b81ff710f736f0d17cc6c92ce707a1a4e8ac1
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2022-03-27 14:49:36 +1100

    test 2

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -12,8 +12,7 @@ const Navbar = () => {
                 <div className='relative group'>
                     <div className='absolute -inset-0.5 bg-gradient-to-r from-sky-400 to-[#5BFDB5] rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt'></div>
                         <a
-                        href='resume.pdf'
-                        target="_blank"
+                        href='/resume.pdf'
                         download
                         className='relative text-gray-100 px-7 py-4 bg-black rounded-lg leading-none flex items-center'>Resume</a>
                 </div>
